0f2c09e587f26539eb65c135d2682c94844ee84c
andrew.flint <andrew.flint@hmrc.gsi.gov.uk>
2016-11-21 09:40:17 +0000

    second commit

---
+++ src/main/java/af/com/test/RestTest1/App.java
@@ -0,0 +1,13 @@
+package af.com.test.RestTest1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
